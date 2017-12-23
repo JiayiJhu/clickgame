0b6a44808f5b12f8e0678e1310d8d5832af4445f
4419-2-1\user <t920245@yahoo.com.tw>
2017-12-23 10:57:48 +0800

    1.新增EnemyBehavior 2.完成MeshFaderer

---
+++ Assets/Scripts/View/Behavior/EnemyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]//attribute屬性  強制約束  強制要掛Animator  Unity內若要移除Animator會跳出警示窗     除非先把這個Script刪掉 才可以刪掉Animator
+public class EnemyBehavior : MonoBehaviour {
+    private Animator animator;
+
+    private void Awake() {                             //只要繼承MonoBehavior 就不用建構子 用Awake()
+        animator = GetComponent<Animator>();           
+        
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/View/Component/MeshFader.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshFader : MonoBehaviour
+{   //淡入淡出
+    public Renderer[] fadeRenderers;
+    public float speed = 1f; //淡入淡出的速度
+
+    //------------------------------測試可不可以執行
+    [ContextMenu("Test FadeIn")]
+    public void TestFadeIn()
+    {
+        StartCoroutine(FadeIn());
+    }
+
+    [ContextMenu("Test FadeOut")]
+    public void TestFadeOut()
+    {
+        StartCoroutine(FadeOut());
+    }
+   // -------------------------------
+
+
+    public IEnumerator FadeIn()
+    {
+        StopCoroutine(FadeOut());
+        float alpha = 0;
+        ChangeAlpha(alpha);
+        while (alpha < 1)
+        {
+            alpha += Time.deltaTime * speed;
+            alpha = Mathf.Min(1,alpha);   //自動從範圍取出最小值
+            ChangeAlpha(alpha);
+            yield return null;
+        }        
+    }
+
+    public IEnumerator FadeOut()
+    {
+        StopCoroutine(FadeIn());
+        float alpha = 1;
+        ChangeAlpha(alpha);
+        while (alpha > 0)
+        {
+            alpha -= Time.deltaTime * speed;
+            alpha = Mathf.Max(0, alpha);   //自動從範圍取出最大值
+            ChangeAlpha(alpha);
+            yield return null;
+        }
+    }
+
+    private void ChangeAlpha(float alpha)   //都會用到 所以可以另外提出來寫 讓其他的呼叫就好
+    {
+        for (int i = 0; i < fadeRenderers.Length; i++)
+        {
+            Color color = fadeRenderers[i].material.color;
+            color.a = alpha;
+            fadeRenderers[i].material.color = color;
+        }
+    }
+}
